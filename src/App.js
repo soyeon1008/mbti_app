c0c3b1f17fd55f58d7a38bf6dd55e9aa0984e6d2
soyeon1008 <kimso4638@naver.com>
2023-09-15 15:34:33 +0900

    23.09.15 mbti_app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import Start from "./pages/Start";
+import { GlobalStyle } from "./components/GlobalStyle";
+import Mbti from "./pages/Mbti";
+import { useSelector } from "react-redux";
+import Show from "./pages/Show";
+
+const Main = styled.main`
+  box-sizing: border-box;
+  width: 100%;
+  max-width: 500px;
+  padding: 0 35px;
+  margin: auto;
+  text-align: center;
+`;
 
 function App() {
+  const page = useSelector((state) => state.mbti.page);
+  const survey = useSelector((state) => state.mbti.survey);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <GlobalStyle />
+      <Main>
+        {page === 0 ? (
+          <Start />
+        ) : page !== survey.length + 1 ? (
+          <Mbti />
+        ) : (
+          <Show />
+        )}
+      </Main>
+    </>
   );
 }
 
---
+++ src/pages/Mbti.jsx
@@ -0,0 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+import SkyblueButton from "../components/SkyblueButton";
+import styled from "styled-components";
+import { check, next } from "../store/modules/mbti";
+import Progress from "../components/Progress";
+
+const Question = styled.p`
+  font-size: 1.5rem;
+  color: #777;
+`;
+export default function Mbti() {
+  const survey = useSelector((state) => state.mbti.survey);
+  const page = useSelector((state) => state.mbti.page);
+  // survey[0] page =1
+  // survey[1] page =2
+  const dispatch = useDispatch();
+  return (
+    <>
+      <Question>{survey[page - 1].question}</Question>
+      <ul>
+        {survey[page - 1].answer.map((el, index) => {
+          return (
+            <li key={index}>
+              <SkyblueButton
+                text={el.text}
+                clickEvent={() => {
+                  dispatch(next());
+                  dispatch(check(el.result));
+                }}
+              />
+              {index === 0 && (
+                <p style={{ fontSize: "2rem", paddingTop: "1.5rem" }}>VS</p>
+              )}
+            </li>
+          );
+        })}
+      </ul>
+      <Progress page={page} maxPage={survey.length} />
+    </>
+  );
+}
